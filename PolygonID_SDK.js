71143acbde4f6a0cf63bf1ce8cbb09dcee3ca60b
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-15 20:24:37 +0900

    request specified

--- PolygonID_SDK.js
+++ PolygonID_SDK.js
@@ -1,16 +1,22 @@
 const { auth, resolver, protocol, loaders, circuits } = require('@iden3/js-iden3-auth')
 
+const org = "Gyre";
+const pos = "Developer"
+
 const proofRequest: protocol.ZKPRequest = {
-    id: 1,
+    id: 1,  // should be unique for every request
     circuitId: 'credentialAtomicQuerySigV2',
     query: {
       allowedIssuers: ['*'],
       type: 'EmployeeConfirmation',
       context: 'https://raw.githubusercontent.com/Cosmodude/Gyre_DID/main/EmployeeSchema.jsonld#EmployeeConfirmation',
       credentialSubject: {
-        birthday: {
-          $lt: 20000101,
+        organization: {
+          $eq: org,
         },
+        position: {
+          $eq: pos
+        }
       },
   },
 };
