4d4bd22daf49cce20e8b9c311ce721e447df1f78
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-15 21:09:47 +0900

    verification script started

--- PolygonID_SDK.js
+++ PolygonID_SDK.js
@@ -1,7 +1,9 @@
-const { auth, resolver, protocol, loaders, circuits } = require('@iden3/js-iden3-auth')
+const { auth, resolver, protocol, loaders, circuits } = require('@iden3/js-iden3-auth');
 
+
+// Request !!!
 const org = "Gyre";
-const pos = "Developer"
+const pos = "Developer";
 
 const proofRequest: protocol.ZKPRequest = {
     id: 1,  // should be unique for every request
@@ -20,4 +22,32 @@ const proofRequest: protocol.ZKPRequest = {
       },
   },
 };
-request.body.scope = [...scope, proofRequest];
+const req = request.body.scope = [...scope, proofRequest];
+
+// Verification !!!
+
+const getRawBody = require('raw-body');
+const keyDIR = "./keys";
+const ethURL = "https://polygon-testnet-rpc.allthatnode.com:8545";  // need the real address
+const contractAddress = "0x134B1BE34911E39A8397ec6289782989729807a4";
+
+const raw = await getRawBody(req);
+const tokenStr = raw.toString().trim();
+
+const verificationKeyloader = new loaders.FSKeyLoader(keyDIR); // verification key loader which is used to fetch the verification keys necessary to verify a zero knowledge proof.
+const sLoader = new loaders.UniversalSchemaLoader('ipfs.io');
+
+const ethStateResolver = new resolver.EthStateResolver( // resolver which is used to fetch the identity state from the State Smart Contract
+    ethURL,
+    contractAddress,
+);
+
+const resolvers: resolver.Resolvers = {  
+    ['polygon:mumbai']: ethStateResolver,
+};
+
+const verifier = new auth.Verifier(  // instance of a Verifier
+verificationKeyloader,
+sLoader, 
+resolvers,
+);
