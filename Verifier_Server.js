c9c66d67e88e5bb5950fa45a2b8f030c03c520e9
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-18 13:37:41 +0900

    API works 1

--- Verifier_Server.js
+++ Verifier_Server.js
@@ -1,10 +1,14 @@
-const express = require('express'); 
-const { auth, resolver, loaders } = require('@iden3/js-iden3-auth');
-const getRawBody = require('raw-body');
-const { GetAuthRequest, Callback } = require ('./Verifier_functions.js');
+import express from 'express'; 
+import { auth, resolver, loaders } from '@iden3/js-iden3-auth';
+import getRawBody from 'raw-body';
+import { GetAuthRequest, Callback }  from './Verifier_functions.js';
 
+    
 const app = express();
 const port = 8080;
+
+// Create a map to store the auth requests and their session IDs
+const requestMap = new Map();
 
 app.get("/api/sign-in", (req, res) => {
     console.log('get Auth Request');
@@ -20,5 +24,4 @@ app.listen(port, () => {
     console.log('server running on port 8080');
 });
 
-// Create a map to store the auth requests and their session IDs
-const requestMap = new Map();
+
